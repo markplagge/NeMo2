b7be61e344736740eb51ac38560a90365f9dc552
Mark Plagge <plaggm@rpi.edu>
2020-06-20 12:17:05 -0400

    update ignore

---
+++ scripts/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='nemo_scripts',
+    version='1',
+    packages=[''],
+    url='',
+    license='',
+    author='Mark Plagge',
+    author_email='',
+    description=''
+)
