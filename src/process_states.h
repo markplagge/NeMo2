a94371e9dfaa2a9670d70e0dad87a20f218909f2
Mark Plagge <plaggm@iMac-2.local>
2020-06-28 13:56:16 -0400

    Squashed 'src/libs/neuro_os/' content from commit 43fe9c7
    git-subtree-dir: src/libs/neuro_os
    git-subtree-split: 43fe9c72491af1caf36828c8c7773f914e60d13b

---
+++ src/process_states.h
@@ -0,0 +1,23 @@
+//
+// Created by Mark Plagge on 3/11/20.
+//
+
+#ifndef SUPERNEMO_PROCESS_STATES_H
+#define SUPERNEMO_PROCESS_STATES_H
+namespace neuro_os {
+    namespace sim_proc {
+        typedef enum {
+            WAITING = 	1 << 0,
+            RUNNING = 	1 << 1,
+            COMPLETE =	1 << 2,
+			PRE_WAIT =  1 << 3,
+			NO_OP = 0
+        } PROC_STATE;
+
+        typedef enum {
+            CAN_ADD,
+            IS_FULL
+        } SCHEDULER_STATE;
+    }
+}
+#endif //SUPERNEMO_PROCESS_STATES_H
