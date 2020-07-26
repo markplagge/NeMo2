f283825a9ce441a084a3040ae7d482088d00e52b
Mark Plagge <plaggm@rpi.edu>
2020-07-25 22:36:39 -0400

    new scheduler desigjn

---
+++ src/nemo/nemo_neuro_system/neurosynaptic_cores/NemoCoreSchedulerLight.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mark Plagge on 7/25/20.
+//
+
+#include "NemoCoreSchedulerLight.h"
---
+++ src/nemo/nemo_neuro_system/neurosynaptic_cores/NemoCoreSchedulerLight.h
@@ -0,0 +1,16 @@
+//
+// Created by Mark Plagge on 7/25/20.
+//
+
+#ifndef NEMOTNG_NEMOCORESCHEDULERLIGHT_H
+#define NEMOTNG_NEMOCORESCHEDULERLIGHT_H
+
+
+
+class NemoCoreSchedulerLight {
+
+};
+
+
+
+#endif //NEMOTNG_NEMOCORESCHEDULERLIGHT_H
